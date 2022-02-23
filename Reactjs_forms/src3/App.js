56376936e7112a37ece06ffa85ddcd90cd4b096d
Karan Meena <87779391+karanmeena237@users.noreply.github.com>
2022-02-24 02:55:41 +0530

    Add files via upload

---
+++ Reactjs_forms/src3/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Link } from 'react-router-dom';
+import LeftBar from './Leftbar';
+import Right from './Right';
+
+function App() {
+  return (
+    <div className="App" style={{"padding":"0","margin":"0","backgroundColor":"#393c49 !important"}}>
+     <LeftBar/>
+    <Right/>
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ Reactjs_forms/src3/Leftbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import image from './Appereance.png'
+function LeftBar() {
+    return (
+      <div className="Sidenavbar" id="sidebar" style={{"position":"fixed","display":"inline-block","backgroundColor":"#1f1d2b","width":"70px","textAlign":"center","height":"100%","top":"0"}}>
+      <nav>
+        <ul className="list-unstyled components">
+          <li className="active text-white">
+            <img src={image} alt="" />
+          </li>
+          <li>
+          <img src={image} alt="" />
+          </li>
+        </ul>
+      </nav>
+    </div>
+     );
+}
+
+export default LeftBar;
